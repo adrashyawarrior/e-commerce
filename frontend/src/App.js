bd5773819dae9e8ec58454870741a66fa73c401c
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-20 16:19:14 +0530

    Routes Use Navbar Basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Navbar from './layouts/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi Lalit, I am working...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>}/>
+          <Route path='/products' element={<h1>Products</h1>}/>
+          <Route path='/users' element={<h1>Users</h1>}/>
+          <Route path='/logout' element={<h1>Logout</h1>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
