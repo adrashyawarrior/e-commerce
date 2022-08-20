ec0113b4bef1ba52de9300b6157692388806b8bb
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-20 16:44:41 +0530

    Adding Tailwind Navbar Styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,14 @@ import Navbar from './layouts/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<h1>Home</h1>}/>
-          <Route path='/products' element={<h1>Products</h1>}/>
-          <Route path='/users' element={<h1>Users</h1>}/>
-          <Route path='/logout' element={<h1>Logout</h1>}/>
+          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/products' element={<h1>Products</h1>} />
+          <Route path='/users' element={<h1>Users</h1>} />
+          <Route path='/logout' element={<h1>Logout</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
