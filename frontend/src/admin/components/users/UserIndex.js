45a0cc104055bc4b269fc40df4e5476d13ef8611
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-21 22:18:06 +0530

    Users Index Done

--- frontend/src/admin/components/users/UserIndex.js
+++ frontend/src/admin/components/users/UserIndex.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const UserIndex = () => {
+    const baseURL = "http://localhost:4000/users";
+    const [users, setUsers] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setUsers(response.data);
+            console.log(users);
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <div className='p-8'>
             <div className='grid grid-cols-2'>
@@ -11,15 +23,15 @@ const UserIndex = () => {
                     </div>
                 </div>
                 <div className='flex flex-row-reverse pb-4'>
-                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-4" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
                         </svg>
                         BACK
                     </button>
-                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-4" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
                         </svg>
                         ADD NEW
                     </button>
@@ -30,23 +42,23 @@ const UserIndex = () => {
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" className="py-3 px-6">
-                                Product name
+                                Full Name
                             </th>
                             <th scope="col" className="py-3 px-6">
                                 <div className="flex items-center">
-                                    Color
+                                    Email
                                     <Link to="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></Link>
                                 </div>
                             </th>
                             <th scope="col" className="py-3 px-6">
                                 <div className="flex items-center">
-                                    Category
+                                    Password
                                     <Link to="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></Link>
                                 </div>
                             </th>
                             <th scope="col" className="py-3 px-6">
                                 <div className="flex items-center">
-                                    Price
+                                    Status
                                     <Link to="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></Link>
                                 </div>
                             </th>
@@ -56,57 +68,29 @@ const UserIndex = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td className="py-4 px-6">
-                                Sliver
-                            </td>
-                            <td className="py-4 px-6">
-                                Laptop
-                            </td>
-                            <td className="py-4 px-6">
-                                $2999
-                            </td>
-                            <td className="py-4 px-6 text-right">
-                                <Link to="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                            </td>
-                        </tr>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Microsoft Surface Pro
-                            </th>
-                            <td className="py-4 px-6">
-                                White
-                            </td>
-                            <td className="py-4 px-6">
-                                Laptop PC
-                            </td>
-                            <td className="py-4 px-6">
-                                $1999
-                            </td>
-                            <td className="py-4 px-6 text-right">
-                                <Link to="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                            </td>
-                        </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Magic Mouse 2
-                            </th>
-                            <td className="py-4 px-6">
-                                Black
-                            </td>
-                            <td className="py-4 px-6">
-                                Accessories
-                            </td>
-                            <td className="py-4 px-6">
-                                $99
-                            </td>
-                            <td className="py-4 px-6 text-right">
-                                <Link to="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                            </td>
-                        </tr>
+                        {
+                            users.map((user, i) => {
+                                return (
+                                    <tr key={`user-${i}`} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {user.name}
+                                        </th>
+                                        <td className="py-4 px-6">
+                                            {user.email}
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            {user.password}
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            {user.status}
+                                        </td>
+                                        <td className="py-4 px-6 text-right">
+                                            <Link to="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
                     </tbody>
                 </table>
             </div>
