032b8e7eddaea9353baea9ef466933fec2cdfa00
lalitwebhopers <lalit.webhopers@gmail.com>
2022-08-31 15:19:20 +0530

    Style Fix

--- frontend/src/admin/components/products/ProductIndex.js
+++ frontend/src/admin/components/products/ProductIndex.js
@@ -35,10 +35,10 @@ const ProductIndex = () => {
                         BACK
                     </Link>
                     <Link to="/products/create" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4" viewBox="0 0 20 20" fill="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
                         </svg>
-                        ADD NEW
+                        NEW PRODUCT
                     </Link>
                 </div>
             </div>
@@ -46,11 +46,14 @@ const ProductIndex = () => {
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col col-span-2" className="py-3 px-6">
+                            <th scope="col" className="py-3 px-6">
                                 Product
                             </th>
-                            <th scope="col col-span-2" className="py-3 px-6">
-                                Title
+                            <th scope="col" className="py-3 px-6">
+                                <div className="flex items-center">
+                                    Title
+                                    <Link to="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></Link>
+                                </div>
                             </th>
                             <th scope="col" className="py-3 px-6">
                                 <div className="flex items-center">
@@ -65,10 +68,7 @@ const ProductIndex = () => {
                                 </div>
                             </th>
                             <th scope="col" className="py-3 px-6">
-                                <span className="sr-only">Edit</span>
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                <span className="sr-only">Delete</span>
+                                Action
                             </th>
                         </tr>
                     </thead>
@@ -77,7 +77,7 @@ const ProductIndex = () => {
                             products.map((product, i) => {
                                 return (
                                     <tr key={`product-${i}`} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <th scope="row" className="py-2 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                             <img className='w-16' alt='product-image' src={'http://localhost:4000/' + product.image} />
                                         </th>
                                         <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -89,11 +89,21 @@ const ProductIndex = () => {
                                         <td className="py-4 px-6">
                                             {product.stock}
                                         </td>
-                                        <td className="py-4 px-6 text-right">
-                                            <Link to="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                                        </td>
-                                        <td className="py-4 px-6 text-right">
-                                            <Link to="#" onClick={() => { deleteProduct(product._id) }} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</Link>
+                                        <td className="py-4 px-6 flex items-center">
+                                            <Link to="#" className="px-1 font-medium inline-block text-blue-600 dark:text-blue-500 hover:underline">
+                                                <span className='text-blue-500'>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                    </svg>
+                                                </span>
+                                            </Link>
+                                            <Link to="#" onClick={() => { deleteProduct(product._id) }} className="px-1 inline-block font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                                <span className='text-red-500'>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                    </svg>
+                                                </span>
+                                            </Link>
                                         </td>
                                     </tr>
                                 )
