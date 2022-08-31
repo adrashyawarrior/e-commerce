539f2c9518b282badccea8a3b15252788656e8d4
lalitwebhopers <lalit.webhopers@gmail.com>
2022-08-31 15:08:58 +0530

    style fix

--- frontend/src/admin/components/products/ProductIndex.js
+++ frontend/src/admin/components/products/ProductIndex.js
@@ -46,8 +46,11 @@ const ProductIndex = () => {
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" className="py-3 px-6">
-                                Product Name
+                            <th scope="col col-span-2" className="py-3 px-6">
+                                Product
+                            </th>
+                            <th scope="col col-span-2" className="py-3 px-6">
+                                Title
                             </th>
                             <th scope="col" className="py-3 px-6">
                                 <div className="flex items-center">
@@ -58,12 +61,6 @@ const ProductIndex = () => {
                             <th scope="col" className="py-3 px-6">
                                 <div className="flex items-center">
                                     Stock
-                                    <Link to="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></Link>
-                                </div>
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                <div className="flex items-center">
-                                    image
                                     <Link to="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></Link>
                                 </div>
                             </th>
@@ -82,6 +79,8 @@ const ProductIndex = () => {
                                     <tr key={`product-${i}`} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                         <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                             <img className='w-16' alt='product-image' src={'http://localhost:4000/' + product.image} />
+                                        </th>
+                                        <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                             {product.name}
                                         </th>
                                         <td className="py-4 px-6">
@@ -89,9 +88,6 @@ const ProductIndex = () => {
                                         </td>
                                         <td className="py-4 px-6">
                                             {product.stock}
-                                        </td>
-                                        <td className="py-4 px-6">
-                                            {product.image}
                                         </td>
                                         <td className="py-4 px-6 text-right">
                                             <Link to="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
