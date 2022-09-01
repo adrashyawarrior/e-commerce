8d49d95a4084924d1bcc97ee4bda72ceb8e8e2f8
lalitwebhopers <lalit.webhopers@gmail.com>
2022-09-01 12:20:23 +0530

    Product Edit Page Created

--- frontend/src/admin/components/products/ProductEdit.js
+++ frontend/src/admin/components/products/ProductEdit.js
@@ -9,7 +9,10 @@ const ProductEdit = () => {
     const [price, setPrice] = useState(0);
     const [stock, setStock] = useState(0);
     const [image, setImage] = useState("");
- 
+    const [file, setFile] = useState("");
+
+    const [fileDataURL, setFileDataURL] = useState(null);
+
     useEffect(() => {
         ProductService.editProduct(id).then((response) => {
             setName(response.name);
@@ -29,10 +32,28 @@ const ProductEdit = () => {
         formData.append('name', name);
         formData.append('price', price);
         formData.append('stock', stock);
-        formData.append('image', image);
+        formData.append('image', file);
         await ProductService.createProduct(formData);
         navigate('/products');
     }
+
+    useEffect(() => {
+        if (file) {
+            if (!file.type.match(/image\/(png|jpg|jpeg)/i)) {
+                alert("Image mime type is not valid");
+                setImage();
+            } else {
+                let fileReader = new FileReader();
+                fileReader.onload = (e) => {
+                    const { result } = e.target;
+                    if (result) {
+                        setFileDataURL(result);
+                    }
+                }
+                fileReader.readAsDataURL(file);
+            }
+        }
+    }, [file]);
 
     return (
         <div className='m-8 p-16 rounded-xl bg-gray-100'>
@@ -59,14 +80,16 @@ const ProductEdit = () => {
                 <div className="mb-6">
                     <label htmlFor="image" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image</label>
                     <input type="file" name='image' id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Robert Junior" required=""
-                        onChange={(e) => { setImage(e.target.files[0]) }}
+                        onChange={(e) => { setImage(e.target.files[0]); setFile(e.target.files[0]) }}
                     />
                 </div>
                 <div className="flex items-start mb-6">
-                    <div className="flex items-center h-5">
-                        <input id="remember" type="checkbox" value="" className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="" />
-                    </div>
-                    <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                    {fileDataURL || image ?
+                        <p className="img-preview-wrapper">
+                            {
+                                <img className='w-32' src={fileDataURL || 'http://localhost:4000/' + image} alt="preview" />
+                            }
+                        </p> : null}
                 </div>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     onClick={submit}
