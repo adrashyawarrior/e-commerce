52e5b01081c212055c86eccac0f87ae06faf285b
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-09-02 17:22:32 +0530

    Create Product Image Fix

--- frontend/src/admin/components/products/ProductCreate.js
+++ frontend/src/admin/components/products/ProductCreate.js
@@ -71,12 +71,18 @@ const ProductCreate = () => {
                     />
                 </div>
                 <div className="mb-6">
-                    <label htmlFor="image" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image</label>
+                    <label htmlFor="categories" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image</label>
                     <Select
                         defaultValue={selectedCategories}
                         onChange={setSelectedOptions}
                         options={categories}
                         isMulti
+                    />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="image" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image</label>
+                    <input type="file" name='image' id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Robert Junior" required=""
+                        onChange={(e) => { setImage(e.target.files[0]) }}
                     />
                 </div>
                 <div className="flex items-start mb-6">
