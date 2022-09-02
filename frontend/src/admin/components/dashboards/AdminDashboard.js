3a15093b8d0b8704e1dbfc6169161fb4445de2a3
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-09-02 09:54:24 +0530

    Some Fix

--- frontend/src/admin/components/dashboards/AdminDashboard.js
+++ frontend/src/admin/components/dashboards/AdminDashboard.js
@@ -55,7 +55,7 @@ const AdminDashboard = () => {
                     <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="about" role="tabpanel" aria-labelledby="about-tab">
                         <h2 className="mb-5 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white">We invest in the world’s potential</h2>
                         {/* List  */}
-                        <ul role="list" className="space-y-4 text-gray-500 dark:text-gray-400">
+                        <ul className="space-y-4 text-gray-500 dark:text-gray-400">
                             <li className="flex space-x-2">
                                 {/* Icon */}
                                 <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
@@ -125,7 +125,6 @@ const AdminDashboard = () => {
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
