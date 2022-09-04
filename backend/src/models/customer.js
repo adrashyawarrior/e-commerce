73a0ff6f9411a9af469c4744a2b8aee57bbcafd4
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-09-04 21:56:48 +0530

    Customer Model Created

---
+++ backend/src/models/customer.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Customer = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Active', 'Inactive'],
+        default: 'Active'
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Customer', Customer);
