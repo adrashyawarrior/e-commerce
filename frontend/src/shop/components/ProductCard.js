932e2820641741fd17290201965ad93ba3017b59
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-09-04 11:22:34 +0530

    style fix

--- frontend/src/shop/components/ProductCard.js
+++ frontend/src/shop/components/ProductCard.js
@@ -16,7 +16,7 @@ const ProductCard = ({ product }) => {
                 <Rating rating={Math.random() * 4 + 1} />
                 <div className="flex justify-between items-center">
                     <span className="text-3xl font-bold text-gray-900 dark:text-white">₹{product.price}</span>
-                    <Link to="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</Link>
+                    <Link to="#" className="text-white bg-blue-700 hover:bg-lime-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</Link>
                 </div>
             </div>
         </div>
