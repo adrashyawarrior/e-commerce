2e773d8737bbb72b2532c7b3dda03fde52efe4cf
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-09-05 11:04:50 +0530

    customer registartion Successfull

--- frontend/src/services/CustomerService.js
+++ frontend/src/services/CustomerService.js
@@ -4,4 +4,8 @@ export default class CustomerService {
     static getCustomers(query = "") {
         return Http.get('account/customers' + query);
     }
+
+    static registerCustomer(data) {
+        return Http.post('customers/register', data);
+    }
 }
--- frontend/src/shop/components/customers/CustomerRegister.js
+++ frontend/src/shop/components/customers/CustomerRegister.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import LOGO from '../../../storage/images/logo.png'
+import CustomerService from '../../../services/CustomerService'
 
 const CustomerRegister = () => {
+
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const data = {
+      name: name,
+      email: email,
+      mobile: mobile,
+      password: password
+    };
+    const customer = await CustomerService.registerCustomer(data);
+    console.log(customer);
+    navigate('/');
+  }
+
   return (
     <section className="bg-gray-200 pb-4 dark:bg-gray-900">
       <div className="flex flex-col items-center px-6 py-8 mx-auto lg:py-0">
@@ -18,23 +41,33 @@ const CustomerRegister = () => {
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
                 <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name *</label>
-                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Lalit Kumar" required="required" />
+                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Lalit Kumar" required="required"
+                  value={name} onChange={(e) => { setName(e.target.value) }}
+                />
               </div>
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email *</label>
-                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="required" />
+                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="required"
+                  value={email} onChange={(e) => { setEmail(e.target.value) }}
+                />
               </div>
               <div>
                 <label htmlFor="mobile" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mobile *</label>
-                <input type="text" name="mobile" id="mobile" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0123456789" required="required" />
+                <input type="text" name="mobile" id="mobile" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0123456789" required="required"
+                  value={mobile} onChange={(e) => { setMobile(e.target.value) }}
+                />
               </div>
               <div>
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password *</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="required" />
+                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="required"
+                  value={password} onChange={(e) => { setPassword(e.target.value) }}
+                />
               </div>
               <div>
                 <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password *</label>
-                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="required" />
+                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="required"
+                  value={confirmPassword} onChange={(e) => { setConfirmPassword(e.target.value) }}
+                />
               </div>
               <div className="flex items-start">
                 <div className="flex items-center h-5">
@@ -44,7 +77,9 @@ const CustomerRegister = () => {
                   <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <Link className="font-medium text-primary-600 hover:underline dark:text-primary-500" to="#">Terms and Conditions</Link></label>
                 </div>
               </div>
-              <button type="submit" className="w-full text-white bg-lime-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+              <button type="submit" className="w-full text-white bg-lime-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                onClick={submit}
+              >Create an account</button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account? <Link to="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
               </p>
