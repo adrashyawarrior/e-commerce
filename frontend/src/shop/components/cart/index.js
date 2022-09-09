c39f58d2f86d543fd83df328dacf0bbcc07703e5
lalitwebhopers <lalit.webhopers@gmail.com>
2022-09-09 17:41:22 +0530

    Check OUt design

--- frontend/src/shop/components/cart/index.js
+++ frontend/src/shop/components/cart/index.js
@@ -20,8 +20,30 @@ const Cart = () => {
                     />)
                 })}
             </div>
-            <div className="col-span-4">
-
+            <div className="col-span-4 px-8 pb-8 pt-4">
+                <div className="text-center text-xl font-bold p-4">Checkout</div>
+                <div className="bg-gray-100 p-8 shadow-md grid grid-rows-1">
+                    <div className="grid grid-cols-12">
+                        <div className="col-span-8 text-right">Products Total</div>
+                        <div className="text-center">:</div>
+                        <div className="col-span-3">Rs. 12000</div>
+                    </div>
+                    <div className="grid grid-cols-12">
+                        <div className="col-span-8 text-right">Discount</div>
+                        <div className="text-center">:</div>
+                        <div className="col-span-3">Rs. 12000</div>
+                    </div>
+                    <div className="grid grid-cols-12">
+                        <div className="col-span-8 text-right">Tax</div>
+                        <div className="text-center">:</div>
+                        <div className="col-span-3">Rs. 12000</div>
+                    </div>
+                    <div className="grid grid-cols-12 border-t-2 pt-1 mt-1 border-gray-400">
+                        <div className="col-span-8 text-right">Payable Amount</div>
+                        <div className="text-center">:</div>
+                        <div className="col-span-3">Rs. 12000</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>)
